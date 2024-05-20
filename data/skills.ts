f89ec874c0770c07edcb8bdad45362273dbf183a
GAKUNEMOTO <nemotogaku1@gmail.com>
2024-05-20 09:50:24 +0900

    fix(skills.ts): fix descriptions

--- data/skills.ts
+++ data/skills.ts
@@ -11,36 +11,36 @@ export const skillData: Skill[] = [
         id:"typescript",
         title:"Typescript",
         icon:"typescript",
-        description:"TypescriptはNextJsでWebアプリケーション開発をしています。"
+        description:"Typescriptはオブジェクト指向プログラムを学ぶために学習にTypescriptを学習しました。今では、TypescriptでNextJsを書いています。"
     },
     {
         id:"react",
         title:"react",
         icon:"react",
-        description:"TypescriptはNextJsでWebアプリケーション開発をしています。"
+        description:"ReactはhooksやReduxの理念を理解することに戸惑いが理ましたが、開発を進めるごとに理解が深まり開発の効率を上げてくれました。"
     },
     {
         id:"next",
         title:"Next.js",
         icon:"next",
-        description:"Next.JsはTypescriptとTailwindでWebsプリケーション開発をしています。"
+        description:"Next.Jsでは、サーバーサイドレンダリングに魅力を感じこのフレームワーク一つで作成を完結できてしまうのでここ最近ではずっとNextJsを使用して開発をしています。"
     },
     {
         id:"three",
         title:"Three.JS",
         icon:"three",
-        description:"Three.Jsは趣味程度で簡単な3D Webサイトを作成しています。"
+        description:"Three.Jsは趣味程度で学習しており、将来的には3D Webアプリケーションの作成に手がけていきたいです。"
     },
     {
         id:"css",
         title:"CSS",
         icon:"css",
-        description:"CSSではHTMLとJacascriptやReactで使用して開発をしています。"
+        description:"CSSではHTMLと共に学んできたこともありその基礎が様々なフレームワークを使用しても困ることなく使える技術を取得しました。"
     },
     {
         id:"tailwind",
         title:"Tailwind",
         icon:"tailwind",
-        description:"TailwindはNext.Jsでよく使って開発しています。"
+        description:"TailwindはNext.Jsでよく使って開発しています。 CSSと比べ速効性があり描きやすいためTailwindを好んで使用しています。"
     },
 ]as const;
